aa5c4647e26681d5db78f11080bbfc56a9b4f528
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-10-01 19:32:12 +0300

    pridejimas??

--- ExamDB/Services/ParduotuvesRepository.cs
+++ ExamDB/Services/ParduotuvesRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ExamDB.Database;
 using ExamDB.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,8 +29,33 @@ namespace ExamDB.Services
             Console.WriteLine("q. Exit");
             var veiksmas = Console.ReadKey().Key;
             if (veiksmas == ConsoleKey.NumPad1) Prisijungti();
-            
-            if (veiksmas == ConsoleKey.NumPad2) Registruotis();
+
+            if (veiksmas == ConsoleKey.NumPad2)
+            {
+                Console.WriteLine("Iveskite varda");
+                string vardas = Console.ReadLine();
+                Console.WriteLine("Iveskite pvarde");
+                string pavarde = Console.ReadLine();
+                Console.WriteLine("Iveskite kompanija");
+                string company = Console.ReadLine();
+                Console.WriteLine("Iveskite adresa");
+                string adresas = Console.ReadLine();
+                Console.WriteLine("Iveskite miesta");
+                string miestas = Console.ReadLine();
+                Console.WriteLine("Iveskite valstija");
+                string valstija = Console.ReadLine();
+                Console.WriteLine("Iveskite sali");
+                string salis = Console.ReadLine();
+                Console.WriteLine("Iveskite postKoda");
+                string pastoKodas = Console.ReadLine();
+                Console.WriteLine("Iveskite telNe");
+                string telNr = Console.ReadLine();
+                Console.WriteLine("Iveskite fax");
+                string fax = Console.ReadLine();
+                Console.WriteLine("Iveskite email");
+                string email = Console.ReadLine();
+                Registruotis(vardas, pavarde, company, adresas, miestas, valstija, salis, pastoKodas, telNr, fax, email);
+            }
             
             if (veiksmas == ConsoleKey.NumPad3) DarbuotojuParinktys();
             
@@ -71,12 +97,27 @@ namespace ExamDB.Services
                 long dainosId = long.Parse(Console.ReadLine());
                 DainaPagalId(dainosId);
             }
-            
-            if (veiksmas == ConsoleKey.NumPad2) DainaPagalPavadinima();
-            
-            if (veiksmas == ConsoleKey.NumPad3) DainosPagalAlbumoId();
-            
-            if (veiksmas == ConsoleKey.NumPad4) DainosPagalAlbumoPavadinima();
+
+            if (veiksmas == ConsoleKey.NumPad2)
+            {
+                Console.WriteLine("Iveskite dainos pavadinima");
+                string dainosPavadinimas = Console.ReadLine();
+                DainaPagalPavadinima(dainosPavadinimas);
+            }
+
+            if (veiksmas == ConsoleKey.NumPad3)
+            {
+                Console.WriteLine("Iveskite albumo ID");
+                long albumoId = long.Parse(Console.ReadLine());
+                DainosPagalAlbumoId(albumoId);
+            }
+
+            if (veiksmas == ConsoleKey.NumPad4)
+            {
+                Console.WriteLine("Iveskite albumo pavadinima");
+                string albumoPavdinimas = Console.ReadLine();
+                DainosPagalAlbumoPavadinima(albumoPavdinimas);
+            }
             
             if (veiksmas == ConsoleKey.Q) PirkimoEkranas();
             
@@ -90,9 +131,27 @@ namespace ExamDB.Services
             Console.WriteLine("3. Statistika (Darbuotojams)");
             Console.WriteLine("q. Back");
             var veiksmas = Console.ReadKey().Key;
-            if (veiksmas == ConsoleKey.NumPad1) KeistiKlientuDuomenis();
-            
-            if (veiksmas == ConsoleKey.NumPad2) PakeistiDainosStatusa();
+            if (veiksmas == ConsoleKey.NumPad1)
+            {
+                Console.WriteLine("Ivesjite Id");
+                long klientoId = long.Parse(Console.ReadLine());
+                Console.WriteLine("Iveskite varda");
+                string vardas = Console.ReadLine();
+                Console.WriteLine("Iveskite pavarde");
+                string pavarde = Console.ReadLine();
+                Console.WriteLine("Iveskite email");
+                string email = Console.ReadLine();
+                KeistiKlientuDuomenis(klientoId, vardas, pavarde, email);
+            }
+
+            if (veiksmas == ConsoleKey.NumPad2)
+            {
+                Console.WriteLine("Iveskite Dainos Id");
+                long dainosId = long.Parse(Console.ReadLine());
+                Console.WriteLine("Iveskite Statusa [Activ] arba [Notactiv]");
+                string status = Console.ReadLine();
+                PakeistiDainosStatusa(dainosId, status);
+            }
             
             if (veiksmas == ConsoleKey.NumPad3) StatistikaDarbuotojams();
             
@@ -161,33 +220,76 @@ namespace ExamDB.Services
             throw new NotImplementedException();
         }
 
-        private void PakeistiDainosStatusa()
+        private void PakeistiDainosStatusa(long id, string status)
         {
-            throw new NotImplementedException();
-        }
-
-        private void KeistiKlientuDuomenis()
-        {
-            DarbuotojuParinktysEkranasKeistiKlientuDuomenis();
-            
-        }
-
-        private void DainosPagalAlbumoPavadinima()
-        {
-            using (var chinContext = new ChinookContext())
+            if (status == "Active")
             {
-                var result = chinContext.Albums.GroupBy(g => g.Title);
+                using var context = new ChinookContext();
+                var track = context.Tracks.Find(id);
+                track.Status = "Active";
+                context.SaveChanges();
+            }
+            else if (status == "Notactive")
+            {
+                using var context = new ChinookContext();
+                var track = context.Tracks.Find(id);
+                track.Status = "Notactive";
+                context.SaveChanges();
             }
         }
 
-        private void DainosPagalAlbumoId()
+        private void KeistiKlientuDuomenis(long id, string vardas, string pavarde, string email)
         {
-            throw new NotImplementedException();
+            DarbuotojuParinktysEkranasKeistiKlientuDuomenis();
+            using var context = new ChinookContext();
+            var customer = context.Customers.Find(id);
+            customer.FirstName = vardas;
+            customer.LastName = pavarde;
+            customer.Email = email;
+            context.SaveChanges();
         }
 
-        private void DainaPagalPavadinima()
+        private void DainosPagalAlbumoPavadinima(string albumoPavadinimas)
         {
-            throw new NotImplementedException();
+            using var context = new ChinookContext();
+            var tracks = context.Tracks
+                .Where(x => x.Status == "Active")
+                .Include(x => x.Genre)
+                .Include(x => x.Album)
+                .Where(x => x.Album.Title == albumoPavadinimas)
+                .ToList();
+            foreach (var track in tracks)
+            {
+                Console.WriteLine($"TrackId {track.TrackId}\n Name {track.Name}\n Genre.Name {track.Genre.Name}\n Composer {track.Composer} "); //{Encoding.Default.GetString(track.UnitPrice)} Eur
+            }
+        }
+
+        private void DainosPagalAlbumoId(long albumoId)
+        {
+            using var context = new ChinookContext();
+            var tracks = context.Tracks
+                .Where(x => x.Status == "Active")
+                .Include(x => x.Genre)
+                .Where(x => x.AlbumId == albumoId)
+                .ToList();
+            foreach (var track in tracks)
+            {
+                Console.WriteLine($"{track.TrackId} {track.Name} {track.Genre.Name} {track.Composer} {Encoding.Default.GetString(track.UnitPrice)} Eur");
+            }
+        }
+
+        private void DainaPagalPavadinima(string pavadinimas)
+        {
+            using var context = new ChinookContext();
+            var result = context.Tracks
+                .Where(x => x.Status == "Active")
+                .Include(x => x.Genre)
+                .Where(x => x.Name == pavadinimas)
+                .ToList();
+            foreach (var track in result)
+            {
+                Console.WriteLine($"{track.TrackId} {track.Name} {track.Genre.Name} {track.Composer}");
+            }
         }
 
         private void DainaPagalId(long trackId)
@@ -196,12 +298,15 @@ namespace ExamDB.Services
             {
                 var result = chinContext.Tracks.Where(x => x.TrackId == trackId).Select(c => new Track
                 {
-                    TrackId = c.TrackId,
+                    
                     Album = c.Album,
                     Name = c.Name,
                 });
 
-                foreach (var item in result) { Console.WriteLine($"{item.TrackId} {item.Album} {item.Name}"); }
+                foreach (var item in result) 
+                { 
+                    Console.WriteLine($" {item.Album} {item.Name}"); 
+                }
             }   
         }
 
@@ -217,13 +322,22 @@ namespace ExamDB.Services
 
         private void IdetiIKrepeeli()
         {
+
             PirkimoEkranasIdetiIKrepseli();
             
         }
 
         private void PerziuretiKataloga()
         {
-            throw new NotImplementedException();
+            using var context = new ChinookContext();
+            var tracks = context.Tracks
+                .Where(x => x.Status == "Active")
+                .Include(x => x.Genre)
+                .ToList();
+            foreach (var track in tracks)
+            {
+                Console.WriteLine($"{track.TrackId} {track.Name} {track.Genre.Name} {track.Composer} {Encoding.Default.GetString(track.UnitPrice)} Eur");
+            }
         }
 
         private void DarbuotojuParinktys()
@@ -236,9 +350,24 @@ namespace ExamDB.Services
             }
         }
 
-        private void Registruotis()
+        private void Registruotis(string vardas, string pavarde, string? company, string? adresas, string? miestas, string? valstija, string? salis, string? pastoKodas, string? telNr, string? fax, string email)
         {
-            throw new NotImplementedException();
+            using var context = new ChinookContext();
+            context.Customers.Add(new Customer
+            {
+                FirstName = vardas,
+                LastName = pavarde,
+                Company = company,
+                Address = adresas,
+                City = miestas,
+                State = valstija,
+                Country = salis,
+                PostalCode = pastoKodas,
+                Phone = telNr,
+                Fax = fax,
+                Email = email
+            });
+            context.SaveChanges();
         }
 
         private void Prisijungti()
